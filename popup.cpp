49658306707ac3bb9ce53fc1976aa3dceea77514
Jonathan Spanos <jonspanos04@outlook.com>
2026-08-07 16:16:50 -0400

    Made the popup prettier 	- Added border outline 	- Increased gap between items 	- Removed horizontal scroll bar 	- Removed word wrap and capped height of item blocks Added support for pressing enter to select desired clipboard 	- Currently need to click into popup for keyboard focus, will 	  fix in future update

--- popup.cpp
+++ popup.cpp
@@ -10,48 +10,85 @@
 
 #include "popup.hpp"
 
-    // Constructor
-    HistoryPopup::HistoryPopup(QClipboard* clipboard, QWidget* parent)
-        : QWidget(parent, Qt::FramelessWindowHint | Qt::Popup)
-    {   
-        // Setup GUI
-        auto* layout = new QVBoxLayout(this);
-        list = new QListWidget(this);
-        layout->addWidget(list);
-        setLayout(layout);
+// Constant
+#define WindowHeight 600
+#define WindowWidth 400
 
-        // Assign selected clipboard history to your clipboard and close the popup
-        connect(list, &QListWidget::itemClicked, this, [=](QListWidgetItem* item){
-            clipboard->setText(item->text());
-            close();
-        });
-        
-        // Set up hotkey to open popup
-        auto* hotkey = new QHotkey(QKeySequence("CTRL+SHIFT+V"), true, this);
-        qDebug() << "Hotkey registered: " << hotkey->isRegistered();
-        connect(hotkey, &QHotkey::activated, this, &HistoryPopup::open);
+// Constructor
+HistoryPopup::HistoryPopup(QClipboard* clipboard, QWidget* parent)
+    : QWidget(parent, Qt::Popup)
+{   
+    setAttribute(Qt::WA_TranslucentBackground, true);
 
-    }
+    setFixedWidth(WindowWidth);
+    setFixedHeight(WindowHeight);
+
+    // Setup GUI
+    auto* layout = new QVBoxLayout(this);
+    layout->setContentsMargins(12, 12, 12, 12); // Margins of text in each row
     
-    void HistoryPopup::updateHistory(const QString str) {
-        if (str.isEmpty()) return; // no empty
-        if (history.contains(str)) {
-            // Bring it to front of list
-            size_t idx = history.indexOf(str);
-            history.move(idx, history.length()-1);
-            return;
-        }
-        history.push_back(str);
-    }
+    list = new QListWidget(this);
+    // Scroll Behavior
+    list->setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
+    list->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    
+    // Selection
+    list->setSelectionMode(QAbstractItemView::SingleSelection);
+    
+    // Style
+    list->setUniformItemSizes(false);
+    list->setWordWrap(false);
+    list->setSpacing(8);
+    list->setStyleSheet("QListWidget::item {border: 2px solid black; border-radius: 8px;} \
+                         QListWidget::item::selected {color: black;}");
 
-    void HistoryPopup::open() {
-        refresh(); // Update popup with current history
-        move(QCursor::pos()); // Ensures popup is placed on cursor
-        show(); // Shows popup
-    }
+    layout->addWidget(list);
 
-    // update list on GUI
-    void HistoryPopup::refresh() {
-        list->clear();
-        for (const auto &item : history) list->addItem(item);
+
+    setLayout(layout);
+
+    // Assign selected clipboard history (click/enter) to your clipboard and close the popup
+    connect(list, &QListWidget::itemClicked, this, [=](QListWidgetItem* item){
+        clipboard->setText(item->text());
+        close();
+    });
+    connect(list, &QListWidget::itemActivated, this, [=](QListWidgetItem* item){
+        clipboard->setText(item->text());
+        close();
+    });
+    
+    // Set up hotkey to open popup
+    auto* hotkey = new QHotkey(QKeySequence("CTRL+SHIFT+V"), true, this);
+    qDebug() << "Hotkey registered: " << hotkey->isRegistered();
+    connect(hotkey, &QHotkey::activated, this, &HistoryPopup::open);
+
+}
+
+// Adds new entry to history / brings old entry up to front if copied again.
+void HistoryPopup::updateHistory(const QString str) {
+    if (str.isEmpty()) return; // no empty
+    if (history.contains(str)) {
+        // Bring it to front of list
+        size_t idx = history.indexOf(str);
+        history.move(idx, history.length()-1);
+        return;
     }
+    history.push_back(str);
+}
+
+// Updates list with new history, places popup at cursor, and shows it for user.
+void HistoryPopup::open() {
+    refresh(); // Update popup with current history
+    move(QCursor::pos()); // Places popup on cursor, at the bottom-left corner
+    show(); // Shows popup
+}
+
+// update list on GUI
+void HistoryPopup::refresh() {
+    list->clear();
+    for (auto i = history.rbegin(); i != history.rend(); i++) {
+        auto* it = new QListWidgetItem(*i);
+        it->setSizeHint(QSize(WindowWidth, WindowHeight/4));
+        list->addItem(it);
+    }
+}
